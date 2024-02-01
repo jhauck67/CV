d88a14114fd99591e885dae0e1f72c1d897c932b
Jhauck <153140378+jhauck67@users.noreply.github.com>
2024-02-01 22:24:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// NAVBAR
+// **** NAVBAR ****
+
+const links = document.querySelectorAll("nav li");
+
+icons.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
